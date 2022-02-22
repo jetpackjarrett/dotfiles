51827a47121baf329631d4a7bb9fc46977964001
Jarrett Gossett <jarrettgossett@gmail.com>
2022-02-21 21:28:18 -0500

    updated homebrew

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@ xcode-select -p || exit "XCode must be installed! (use the app store)";
 
 echo "$(tput setaf 2)Installing homebrew..."
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 echo "$(tput setaf 2)Cloning dotfiles...$(tput sgr 0)"
 git clone https://github.com/jetpackjarrett/dotfiles.git ~/.dotfiles
