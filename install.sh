aa3336bcb5ab45b1e13a3a616e37f4b5d00d93b8
Jarrett Gossett <jarrettgossett@gmail.com>
2023-02-09 09:58:14 -0500

    install

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 #!/bin/zsh
-# # Only run if the tools are not installed yet
-# # To check that try to print the SDK path
-# xcode-select -p &> /dev/null
-# if [ $? -ne 0 ]; then
-#   echo "Command Line Tools for Xcode not found. Installing from softwareupdate..."
-# # This temporary file prompts the 'softwareupdate' utility to list the Command Line Tools
-#   touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
-#   PROD=$(softwareupdate -l | grep "\*.*Command Line" | tail -n 1 | sed 's/^[^C]* //')
-#   softwareupdate -i "$PROD" --verbose;
-# else
-#   echo "Command Line Tools for Xcode have been installed."
-# fi
+# Only run if the tools are not installed yet
+# To check that try to print the SDK path
+xcode-select -p &> /dev/null
+if [ $? -ne 0 ]; then
+  echo "Command Line Tools for Xcode not found. Installing from softwareupdate..."
+# This temporary file prompts the 'softwareupdate' utility to list the Command Line Tools
+  touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
+  PROD=$(softwareupdate -l | grep "\*.*Command Line" | tail -n 1 | sed 's/^[^C]* //')
+  softwareupdate -i "$PROD" --verbose;
+else
+  echo "Command Line Tools for Xcode have been installed."
+fi
 
 which -s brew
 if [[ $? != 0 ]] ; then
@@ -24,7 +24,13 @@ else
 fi
 
 
-# echo "$(tput setaf 2)Running setup...$(tput sgr 0)"
-# echo ' '
-# sh setup.sh
+echo "$(tput setaf 2)Cloning dotfiles...$(tput sgr 0)"
+git clone git@github.com:jetpackjarrett/dotfiles.git ~/.dotfiles
+cd ~/.dotfiles
+
+echo "$(tput setaf 2)Running setup...$(tput sgr 0)"
+echo ' '
+sh setup.sh
+
+
 
