b22e90797eac621dfcf751c748c3c2a664f0e227
Jarrett Gossett <jarrettgossett@gmail.com>
2023-02-09 09:55:29 -0500

    testing

--- README.md
+++ README.md
@@ -9,18 +9,6 @@
 1. Clone this repo: `git clone https://github.com/jetpackjarrett/dotfiles.git ~/.dotfiles`
 1. Run `sh ~/.dotfiles/install.sh`
 
-### Locally (Already cloned)
-
-```bash
-sh install.sh
-```
-
-## Usage
-
-## Install on a fresh machine
-
-```bash
-sh first_setup.sh
-```
+### Usage
 
 Feel free to fork and use however you want. Several portions are probably only usable if you are an OSX user. Probably also want to change gitconfig in your fork to your information.
--- install.sh
+++ install.sh
@@ -1,15 +1,30 @@
-xcode-select -p || exit "XCode must be installed! (use the app store)";
+#!/bin/zsh
+# # Only run if the tools are not installed yet
+# # To check that try to print the SDK path
+# xcode-select -p &> /dev/null
+# if [ $? -ne 0 ]; then
+#   echo "Command Line Tools for Xcode not found. Installing from softwareupdate..."
+# # This temporary file prompts the 'softwareupdate' utility to list the Command Line Tools
+#   touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
+#   PROD=$(softwareupdate -l | grep "\*.*Command Line" | tail -n 1 | sed 's/^[^C]* //')
+#   softwareupdate -i "$PROD" --verbose;
+# else
+#   echo "Command Line Tools for Xcode have been installed."
+# fi
 
-echo "$(tput setaf 2)Installing homebrew..."
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
-eval "$(/opt/homebrew/bin/brew shellenv)"
+which -s brew
+if [[ $? != 0 ]] ; then
+  echo "$(tput setaf 2)Installing homebrew..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+else
+  echo "$(tput setaf 2)Updating homebrew..."
+  brew update
+fi
 
-echo "$(tput setaf 2)Cloning dotfiles...$(tput sgr 0)"
-git clone git@github.com:jetpackjarrett/dotfiles.git ~/.dotfiles
-cd ~/.dotfiles
 
-echo "$(tput setaf 2)Running setup...$(tput sgr 0)"
-echo ' '
-sh setup.sh
+# echo "$(tput setaf 2)Running setup...$(tput sgr 0)"
+# echo ' '
+# sh setup.sh
 
